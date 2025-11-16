8d661197f042a187c11a46d58a198d0f7aa17def
Tushar Konala <tusharkonala284@gmail.com>
2025-11-16 15:41:36 +0530

    Installing mongoose

--- routes/genres.js
+++ routes/genres.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 const genres = [
   { id: 1, name: "Action" },
-  { id: 2, name: "Comedy" },
-  { id: 3, name: "Horror" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Romance" },
 ];
 
 router.get("/", (req, res) => {
