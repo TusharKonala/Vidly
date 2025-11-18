316b8e288befc8712d94014ca8ba3b3fc4930eaf
Tushar Konala <tusharkonala284@gmail.com>
2025-11-18 16:46:29 +0530

    Add movie model and routes for CRUD operations

--- models/genre.js
+++ models/genre.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const Genre = mongoose.model(
-  "Genre",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 3,
-      maxlength: 50,
-    },
-  })
-);
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+});
+
+const Genre = mongoose.model("Genre", genreSchema);
 
 function validateGenre(genre) {
   const schema = {
@@ -23,3 +22,4 @@ function validateGenre(genre) {
 
 exports.Genre = Genre;
 exports.validate = validateGenre;
+exports.genreSchema = genreSchema;
