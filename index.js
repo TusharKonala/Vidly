0526a9013b32e9c7d63dd645470043504fb7fbfb
Tushar Konala <tusharkonala284@gmail.com>
2025-11-21 16:23:47 +0530

    Refactor application structure by moving route handling to a dedicated startup module

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const error = require("./middleware/error");
 const winston = require("winston");
 require("winston-mongodb");
 const config = require("config");
@@ -6,12 +5,7 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
+require("./startup/routes")(app);
 const mongoose = require("mongoose");
 
 winston.add(
@@ -61,15 +55,6 @@ mongoose
   .connect("mongodb://localhost/vidly")
   .then(() => console.log('Connecting to the database "vidly"...'))
   .catch((err) => console.log(err.message));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
