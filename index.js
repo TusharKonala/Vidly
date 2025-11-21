be6f154e202515839247eb0c119de83622e6ce14
Tushar Konala <tusharkonala284@gmail.com>
2025-11-21 17:47:35 +0530

    Refactor application startup by modularizing logging, database connection, and configuration handling

--- index.js
+++ index.js
@@ -1,62 +1,16 @@
 const winston = require("winston");
-require("winston-mongodb");
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
+
+// imported logging logic before the db connection so that if any error occurs
+// in db it can be successfully logged
+require("./startup/logging")();
 require("./startup/routes")(app);
-const mongoose = require("mongoose");
-
-winston.add(
-  new winston.transports.File({
-    filename: "logfile.log",
-    handleExceptions: true,
-    handleRejections: true,
-  })
-);
-
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://localhost/vidly",
-    level: "error",
-    handleExceptions: true,
-    handleRejections: true,
-  })
-);
-
-process.on("uncaughtException", (ex) => {
-  console.log("WE GOT AN UNCAUGHT EXCEPTION");
-  // winston.error(ex.message, ex);
-  // added handleExceptions: true to winston.add below
-  //used timeout so that we can allow winston to to log the uncaught exception
-
-  setTimeout(() => process.exit(1), 200);
-});
-
-process.on("unhandledRejection", (ex) => {
-  console.log("WE GOT AN UNHANDLED REJECTION");
-  // winston.error(ex);
-  //used timeout so that we can allow winston to log the rejected promise
-  setTimeout(() => process.exit(1), 200);
-});
-
-// throw new Error("Something failed during startup...");
-
-const p = Promise.reject(new Error("Something failed miserably.."));
-p.then(() => console.log("Done"));
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
-
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log('Connecting to the database "vidly"...'))
-  .catch((err) => console.log(err.message));
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening at port: ${port}`);
+  winston.info(`Listening at port: ${port}`);
 });
---
+++ startup/logging.js
@@ -0,0 +1,41 @@
+const winston = require("winston");
+require("winston-mongodb");
+
+module.exports = function () {
+  winston.rejections.handle(
+    new winston.transports.File({ filename: "uncaughtRejections.log" })
+  );
+  // handles uncaught rejected promises
+
+  winston.exceptions.handle(
+    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+  );
+  // handles uncaught exception ie throw new Error(), etc
+
+  winston.add(
+    new winston.transports.File({
+      filename: "logfile.log",
+    })
+  );
+
+  winston.add(
+    new winston.transports.MongoDB({
+      db: "mongodb://localhost/vidly",
+      level: "error",
+    })
+  );
+
+  process.on("uncaughtException", (ex) => {
+    console.log("WE GOT AN UNCAUGHT EXCEPTION");
+
+    //used timeout so that we can allow winston to to log the uncaught exception
+    setTimeout(() => process.exit(1), 200);
+  });
+
+  process.on("unhandledRejection", (ex) => {
+    console.log("WE GOT AN UNHANDLED REJECTION");
+
+    //used timeout so that we can allow winston to log the rejected promise
+    setTimeout(() => process.exit(1), 200);
+  });
+};
