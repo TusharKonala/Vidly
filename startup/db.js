384650ae1b4fe557db05753dd96906222f1a78f2
Tushar Konala <tusharkonala284@gmail.com>
2025-11-25 16:58:07 +0530

    Adding logs for connection error

--- startup/db.js
+++ startup/db.js
@@ -4,8 +4,11 @@ const config = require("config");
 
 module.exports = function () {
   const db = config.get("db");
-  mongoose.connect(db).then(() => {
-    winston.info(`Connected to ${db}...`);
-    console.log(`Connected to ${db}...`);
-  });
+  mongoose
+    .connect(db)
+    .then(() => {
+      winston.info(`Connected to ${db}...`);
+      console.log(`Connected to ${db}...`);
+    })
+    .catch((err) => console.error("Connection failure: ", err));
 };
